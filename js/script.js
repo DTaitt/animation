d9a99660004872b55f193745d63368a70e034e8e
DTaitt <Donovan.taitt@yahoo.com>
2017-03-15 14:38:09 -0400

    redid complete project

--- js/script.js
+++ js/script.js
@@ -1,14 +1,79 @@
 function Move () {
-	var bb8 = $("#bb8");
-	var pos = -210;
-	var id = setInterval(frame, 1);
-	function frame () {
-		if (pos === 900) {
-			clearInterval(id);
-			$("#container").removeClass("initial_bg").addClass("second_bg")
-		} else {
-			pos ++;
-			bb8.css("left", pos + "px")
+
+	setInterval(blue, 2000);
+	setInterval(yellow, 1000);
+	setInterval(red, 500);
+	setInterval(green, 400);
+
+	function blue () {
+		var blue = $("#blue");
+		var posX = 0;
+		var posY = 0;
+		var id = setInterval(frame, 10);
+		function frame () {
+			if (posX === 525) {
+				clearInterval(id);
+				$("#outer").css("background", "blue")
+			} else {
+				posX ++;
+				posY ++;
+				blue.css("left", posX + "px")
+				blue.css("top", posY + "px")
+			}
 		}
 	}
+	function yellow () {
+		var yellow = $("#yellow");
+		var posX = 525;
+		var posY = 525;
+		var id = setInterval(frame, 20);
+		function frame () {
+			if (posX === 0) {
+				clearInterval(id);
+				$("#outer").css("background", "yellow")
+			} else {
+				posX --;
+				posY --;
+				yellow.css("left", posX + "px")
+				yellow.css("top", posY + "px")
+			}
+		}
+	}
+	function red () {
+		var red = $("#red");
+		var posX = 0;
+		var posY = 525;
+		var id = setInterval(frame, 30);
+		function frame () {
+			if (posX === 525) {
+				clearInterval(id);
+				$("#outer").css("background", "red")
+			} else {
+				posX ++;
+				posY --;
+				red.css("left", posX + "px")
+				red.css("top", posY + "px")
+			}
+		}
+	}
+	function green () {
+		var green = $("#green");
+		var posX = 525;
+		var posY = 0;
+		var id = setInterval(frame, 40);
+		function frame () {
+			if (posX === 0) {
+				clearInterval(id);
+				$("#outer").css("background", "green")
+			} else {
+				posX --;
+				posY ++;
+				green.css("left", posX + "px")
+				green.css("top", posY + "px")
+			}
+		}
+	}
+	blue()
+	yellow()
+	red()
 }
